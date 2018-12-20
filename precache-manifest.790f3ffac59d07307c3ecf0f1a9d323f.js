f32b3cadcf5264f842febd3e4fd03dd6e8fb3fb5
Frederik <fh@frederikhaaning.dk>
2018-12-20 15:04:49 +0100

    Trying again..

---
+++ precache-manifest.790f3ffac59d07307c3ecf0f1a9d323f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "94ecc7b693b2736ea2a0",
+    "url": "./static/js/main.94ecc7b6.chunk.js"
+  },
+  {
+    "revision": "ed4be6d3e42c4f8840a4",
+    "url": "./static/js/1.ed4be6d3.chunk.js"
+  },
+  {
+    "revision": "94ecc7b693b2736ea2a0",
+    "url": "./static/css/main.4c64cc31.chunk.css"
+  },
+  {
+    "revision": "ed4be6d3e42c4f8840a4",
+    "url": "./static/css/1.abc81be9.chunk.css"
+  },
+  {
+    "revision": "50575f68a4fe540f0f0f6adb9ca800df",
+    "url": "./index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7a7108a63fc60befbdf8fb6c59044971.js"
+  "./precache-manifest.790f3ffac59d07307c3ecf0f1a9d323f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
